442e85ec4941c6d789da0b9f66359f1995688308
Kimber Ogden <mx.kimber@gmail.com>
2023-08-25 14:13:48 -0500

    Add reviews model with seeds for initial data

---
+++ db/migrate/20230825190834_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.integer :reviewer_id
+      t.integer :rating
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,11 @@
-Reviewer.create(
-  source: "google",
-  uid: "google_uid_123",
-  email: "user1@test.com",
-  name: "Bert"
+Review.create(
+  reviewer_id: 2,
+  rating: 5,
+  comment: "great pet sitter!"
 )
 
-Reviewer.create(
-  source: "facebook",
-  uid: "fb_uid_456",
-  email: "user2@test.com",
-  name: "Ernie"
+Review.create(
+  reviewer_id: 1,
+  rating: 5,
+  comment: "Best experience for my pup!"
 )

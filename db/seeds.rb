65da3516776d3630d0cdbfa19d77287a865aefed
Kimber Ogden <mx.kimber@gmail.com>
2023-08-24 23:27:53 -0500

    Add useres model and seed for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
-ServiceCatalog.create(service: "Dog Sitting", price: 60)
-ServiceCatalog.create(service: "Cat Sitting", price: 50)
+User.create(
+  first_name: "Tiffany",
+  last_name: "Mohr",
+  email: "tiff@admin.com",
+  phone_number: "555-555-5555",
+  password_digest: "password",
+  profile_photo: "uploaded.jpeg",
+  admin_permission: true,
+  bio: "Hi, I'm Tiff and I've been..."
+)

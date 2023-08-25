ca8933aca615adab672dfc33e6ea1437e2f2d30e
Kimber Ogden <mx.kimber@gmail.com>
2023-08-25 12:55:32 -0500

    Add Photos model and seed for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-User.create(
-  first_name: "Tiffany",
-  last_name: "Mohr",
-  email: "tiff@admin.com",
-  phone_number: "555-555-5555",
-  password_digest: "password",
-  profile_photo: "uploaded.jpeg",
-  admin_permission: true,
-  bio: "Hi, I'm Tiff and I've been..."
+Photo.create(
+  pet_photo: "https://i2-prod.mirror.co.uk/incoming/article14241558.ece/ALTERNATES/n615/1_SWNS_FOSSIL_DOG_06.jpg",
+  pet_name: "Bingo",
+  caption: "Is this a dino bone?"
+)
+
+Photo.create(
+  pet_photo: "https://i.pinimg.com/originals/92/36/ce/9236ce7720fab54dfffed9be976cd1d6.jpg",
+  pet_name: "Sparky",
+  caption: "King Sparky looking dapper!"
+)
+
+Photo.create(
+  pet_photo: "https://i.ytimg.com/vi/LMZg_rd6mec/hqdefault.jpg",
+  pet_name: "Chilly",
+  caption: "She loves her new toy."
 )

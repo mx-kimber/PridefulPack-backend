e86c38a8f23acaca531cd7b660b9d6b186c6b6d1
Kimber Ogden <mx.kimber@gmail.com>
2023-08-26 22:19:28 -0500

    Add create action with test for service_offerings, update jbuilder to reflect the drop of the extra_service_price column

--- app/controllers/service_offerings_controller.rb
+++ app/controllers/service_offerings_controller.rb
@@ -3,5 +3,23 @@ class ServiceOfferingsController < ApplicationController
     @service_offerings= ServiceOffering.all
     render :index
   end
-end
 
+  def create
+    # note to self: create a table to have more than one extra_service
+
+    @service_offering = ServiceOffering.create(
+      service: params[:service],
+      duration: params[:duration],
+      price: params[:price],
+      included: params[:included],
+      extra_service: params[:extra_service]
+    )
+
+    @service_offering.save
+    
+    render :show
+  end
+  
+  
+  
+end
--- test/controllers/service_offerings_controller_test.rb
+++ test/controllers/service_offerings_controller_test.rb
@@ -8,4 +8,21 @@ class ServiceOfferingsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ServiceOffering.count, data.length
   end
+
+  test "create" do
+  #note to self: set instance to accept the array and join
+    @extra_service = ["bathing/nail trims", "etc"]
+    
+    assert_difference "ServiceOffering.count", 1 do
+      post "/service_offerings.json", params: {
+        service: "Dog Sitting - in home",
+        duration: "one night",
+        price: "$60",
+        included: "taking trash out/mail in/bath and nail trim?",
+        extra_service: @extra_service.join(',')
+      }
+      
+      assert_response 200
+    end
+  end
 end

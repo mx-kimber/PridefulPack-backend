a327c8e15cdfb969c6cc53554cca79a6d86d152b
Kimber Ogden <mx.kimber@gmail.com>
2023-08-26 22:50:36 -0500

    Add create action for users, add test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @users = User.all
     render :index
   end
+
+  def create
+
+    @user = User.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      password_digest: params[:password_digest],
+      admin_permission: params[:admin_permission],
+      bio: params[:bio],
+    )
+
+    @user.save
+    
+    render :show
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,4 +8,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Photo.count, data.length
   end
+
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { 
+        first_name: 4, 
+        last_name: 4, 
+        email: "admin2@admin.com",
+        phone_number: "555-555-5555",
+        password_digest: "PW",
+        profile_photo: "photoURL",
+        admin_permission: true,
+        bio: "all about me" 
+      }
+      assert_response 200
+    end
+  end
 end

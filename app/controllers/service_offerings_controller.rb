d106678ed18e38ad6e0c6a432354bb6c17a438e6
Kimber Ogden <mx.kimber@gmail.com>
2023-08-30 21:51:32 -0500

    Refactor ServiceOffernings controller to use strong params

--- app/controllers/service_offerings_controller.rb
+++ app/controllers/service_offerings_controller.rb
@@ -1,20 +1,12 @@
 class ServiceOfferingsController < ApplicationController
   def index
-    @service_offerings= ServiceOffering.all
+    @service_offerings = ServiceOffering.all
     render :index
   end
 
   def create
     # note to self: create a table to have more than one extra_service
-
-    @service_offering = ServiceOffering.create(
-      service: params[:service],
-      price: params[:price],
-      duration: params[:duration],
-      included: params[:included],
-      extra_service: params[:extra_service]
-    )
-
+    @service_offering = ServiceOffering.create(service_offering_params)
     @service_offering.save
     
     render :show
@@ -27,13 +19,7 @@ class ServiceOfferingsController < ApplicationController
 
   def update
     @service_offering = ServiceOffering.find_by(id: params[:id])
-    @service_offering.update(
-      service: params[:service] || @service_offering.service,
-      price: params[:price] || @service_offering.price,
-      duration: params[:duration] || @service_offering.duration,
-      included: params[:included] || @service_offering.included,
-      extra_service: params[:extra_service] || @service_offering.extra_service,
-    )
+    @service_offering.update(service_offering_params)
     render :show
   end
 
@@ -43,4 +29,9 @@ class ServiceOfferingsController < ApplicationController
     render json: { message: "service_offering destroyed successfully" }
   end
   
+  private
+
+  def service_offering_params
+    params.permit(:service, :price, :duration, :included, :extra_service)
+  end
 end

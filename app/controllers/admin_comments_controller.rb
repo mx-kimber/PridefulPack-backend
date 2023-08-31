a6a244f01b98554cf19d33d2dc8c0b56bcf549f2
Kimber Ogden <mx.kimber@gmail.com>
2023-08-30 21:47:11 -0500

    Refactor AdminComments controller to use strong params

--- app/controllers/admin_comments_controller.rb
+++ app/controllers/admin_comments_controller.rb
@@ -1,16 +1,11 @@
 class AdminCommentsController < ApplicationController
-
   def index
     @admin_comments = AdminComment.all
     render :index
   end
 
   def create
-    @admin_comment = AdminComment.create(
-     review_id: params[:review_id],
-      user_id: params[:user_id],
-      comment: params[:comment],
-    )
+    @admin_comment = AdminComment.create(admin_comment_params)
     render :show
   end
 
@@ -21,11 +16,7 @@ class AdminCommentsController < ApplicationController
 
   def update
     @admin_comment = AdminComment.find_by(id: params[:id])
-    @admin_comment.update(
-      review_id: params[:review_id] || @admin_comment.review_id,
-      user_id: params[:user_id] || @admin_comment.user_id,
-      comment: params[:comment] || @admin_comment.comment,
-    )
+    @admin_comment.update(admin_comment_params)
     render :show
   end
 
@@ -33,5 +24,11 @@ class AdminCommentsController < ApplicationController
     @admin_comment = AdminComment.find_by(id: params[:id])
     @admin_comment.destroy
     render json: { message: "admin comment destroyed successfully" }
+  end
+
+  private
+
+  def admin_comment_params
+    params.permit(:review_id, :user_id, :comment)
   end
 end

93821c08c28de1feda77953d57adc4b2b346f807
Kimber Ogden <mx.kimber@gmail.com>
2023-09-13 20:48:23 -0500

    Refactor AdminCommentsController for Improved User Permissions Handling

--- app/controllers/admin_comments_controller.rb
+++ app/controllers/admin_comments_controller.rb
@@ -1,5 +1,6 @@
 class AdminCommentsController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
+  before_action :set_admin_comment, only: [:show, :update, :destroy]
 
   def index
     @admin_comments = AdminComment.all
@@ -7,30 +8,55 @@ class AdminCommentsController < ApplicationController
   end
 
   def create
-    @admin_comment = AdminComment.create(admin_comment_params)
-    render :show
+    @admin_comment = current_user.admin_comments.build(admin_comment_params)
+    if @admin_comment.save
+      render :show
+    else
+      render json: { errors: @admin_comment.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
-    @admin_comment = AdminComment.find_by(id: params[:id])
     render :show
   end
 
   def update
-    @admin_comment = AdminComment.find_by(id: params[:id])
-    @admin_comment.update(admin_comment_params)
-    render :show
+    if can_update_or_destroy?
+      if @admin_comment.update(admin_comment_params)
+        render :show
+      else
+        render json: { errors: @admin_comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render_permission_denied
+    end
   end
 
   def destroy
-    @admin_comment = AdminComment.find_by(id: params[:id])
-    @admin_comment.destroy
-    render json: { message: "admin comment destroyed successfully" }
+    if can_update_or_destroy?
+      @admin_comment.destroy
+      render json: { message: "Admin comment destroyed successfully" }
+    else
+      render_permission_denied
+    end
   end
 
   private
 
   def admin_comment_params
-    params.permit(:review_id, :user_id, :comment)
+    params.permit(:review_id, :comment)
+  end
+
+  def set_admin_comment
+    @admin_comment = AdminComment.find_by(id: params[:id])
+    render json: { error: 'Admin comment not found' }, status: :not_found unless @admin_comment
+  end
+
+  def can_update_or_destroy?
+    @admin_comment.user == current_user || current_user.admin_permission
+  end
+
+  def render_permission_denied
+    render json: { error: 'Permission denied' }, status: :forbidden
   end
 end

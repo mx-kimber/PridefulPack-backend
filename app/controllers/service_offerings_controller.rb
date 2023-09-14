a0e2ab001c49f583a5974ded0294ad441ef5ddd3
Kimber Ogden <mx.kimber@gmail.com>
2023-09-13 23:16:18 -0500

    Refactor ServiceOfferings  to Include User Authentication and Improve Error Handling

--- app/controllers/service_offerings_controller.rb
+++ app/controllers/service_offerings_controller.rb
@@ -1,5 +1,6 @@
 class ServiceOfferingsController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
+  before_action :set_service_offering, only: [:show, :update, :destroy]
 
   def index
     @service_offerings = ServiceOffering.all
@@ -7,32 +8,47 @@ class ServiceOfferingsController < ApplicationController
   end
 
   def create
-    @service_offering = ServiceOffering.create(service_offering_params)
-    @service_offering.save
-    
-    render :show
+    @service_offering = current_user.service_offerings.build(service_offering_params)
+    if @service_offering.save
+      render :show
+    else
+      render json: { errors: @service_offering.errors.full_messages }, status: :unprocessable_entity
+    end
   end
-  
+
   def show
-    @service_offering = ServiceOffering.find_by(id: params[:id])
     render :show
   end
 
   def update
-    @service_offering = ServiceOffering.find_by(id: params[:id])
-    @service_offering.update(service_offering_params)
-    render :show
+    if @service_offering.user_id == current_user.id && @service_offering.update(service_offering_params)
+      render :show
+    else
+      render json: { errors: @service_offering.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @service_offering = ServiceOffering.find_by(id: params[:id])
-    @service_offering.destroy
-    render json: { message: "service_offering destroyed successfully" }
+    if @service_offering.user_id == current_user.id
+      @service_offering.destroy
+      render json: { message: "Service offering destroyed successfully" }
+    else
+      render_permission_denied
+    end
   end
-  
+
   private
 
   def service_offering_params
-    params.permit(:service, :price, :duration, :included, :extra_service)
+    params.permit(:user_id, :service, :price, :duration, :included, :extra_service)
+  end
+
+  def set_service_offering
+    @service_offering = ServiceOffering.find_by(id: params[:id])
+    render json: { error: 'Service offering not found' }, status: :not_found unless @service_offering
+  end
+
+  def render_permission_denied
+    render json: { error: 'Permission denied' }, status: :forbidden
   end
 end

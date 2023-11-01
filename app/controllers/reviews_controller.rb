1c03970cdf004357b0bb68144b959da36509a2ac
Kimber Ogden <mx.kimber@gmail.com>
2023-11-01 17:47:36 -0500

    Install Active Storage ('rails active_storage:install') and migrate

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user, only: [:destroy]
+  before_action :authenticate_user, only: [:destroy, :update]
   
   def index
     @reviews = Review.all
